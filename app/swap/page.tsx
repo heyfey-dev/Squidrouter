c8f8792de49a6ec3c55e91b66ef1836eafea6582
Your New Username <danieokandeji@gmail.com>
2024-09-19 23:35:00 +0100

    first

---
+++ app/swap/page.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../components/swap/Sidebar"
+import Section from "../../components/swap/Section"
+
+
+
+const pagee: React.FC = () => {
+  return (
+    <div>
+        <Sidebar/>
+        <Section/>
+    </div>
+  );
+};
+export default pagee;
