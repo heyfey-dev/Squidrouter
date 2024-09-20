15a987ab975f23181bd845d32e7577b9b7229b2e
Your New Username <danieokandeji@gmail.com>
2024-09-20 10:19:03 +0100

    another error

--- components/swap/Pay.tsx
+++ components/swap/Pay.tsx
@@ -1,18 +1,25 @@
+import React from 'react';
+
+
 export default function Pay() {
   return (
     <div>
       <h1>Pay</h1>
-      <div className="flex overflow-hidden">
+      <div className="flex overflow-hidden space-x-2">
         <div className="flex items-center justify-center text-lg w-10 h-10 p-1 rounded-full border-2 ">
           o
         </div>
-        <div className="flex items-center ">
+        <div className="flex items-center">
           <div className="w-5 h-3 border-2 rounded-full"></div>
         </div>
-        <div className="flex items-center justify-center text-lg w-32 h-10 p-1 rounded-full border-2 "></div>
+        <div className="flex items-center justify-center text-lg w-32 h-10 p-1 rounded-full border-2 ">
+          {/* Add placeholder or content */}
+          Content
+        </div>
       </div>
 
-      <Input type="number" placeholder="0" className=""/>
+      {/* Input component should be defined or imported correctly */}
+      <input type="number" placeholder="0" className="border-2 rounded-md p-2 mt-4" />
     </div>
   );
 }
